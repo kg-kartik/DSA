7168fdfce22d5123adc5737311f40a55fb108ff4
Kartik Goel <goel.kartik39@gmail.com>
2020-11-14 17:07:29 +0530

    Added solutions

--- Novlong/Restore.cpp
+++ Novlong/Restore.cpp
@@ -3,15 +3,41 @@ using namespace std;
 
 int main()
 {
-    long long t;
+    long long t, i, p;
     cin >> t;
+
+    long long limit = 1000000;
+    vector<long long> result;
+
+    bool prime[limit + 1];
+    memset(prime, true, sizeof(prime));
+
+    for (p = 2; p * p <= limit; p++)
+    {
+        if (prime[p] == true)
+        {
+            for (i = p * p; i <= limit; i += p)
+            {
+                prime[i] = false;
+            }
+        }
+    }
+
+    for (p = 2; p <= limit; p++)
+    {
+        if (prime[p] == true)
+        {
+            result.push_back(p);
+        }
+    }
+
     while (t--)
     {
-        long long n, i, x;
+        long long n, x;
         cin >> n;
 
         vector<long long> v;
-        vector<long long> result;
+        vector<long long> output;
 
         for (i = 0; i < n; i++)
         {
@@ -19,41 +45,20 @@ int main()
             v.push_back(x);
         }
 
-        long long limit = 1000000;
-
-        bool prime[limit + 1];
-        memset(prime, true, sizeof(prime));
-
-        for (int p = 2; p * p <= limit; p++)
+        for (i = 0; i < n; i++)
         {
-            if (prime[p] == true)
-            {
-                for (i = p * p; i <= limit; i += p)
-                {
-                    prime[i] = false;
-                }
-            }
-        }
-
-        for (int p = 2; p <= limit; p++)
-        {
-            if (prime[p] == true)
-            {
-                result.push_back(p);
-            }
+            output.push_back(result[i]);
         }
 
         for (i = 0; i < n; i++)
         {
-            if (v[i] != i + 1)
-            {
-                result[i] = result[n - 1];
-            }
+            output[i] = output[v[i] - 1];
         }
 
         for (i = 0; i < n; i++)
         {
-            cout << result[i] << " " << endl;
+            cout << output[i] << " ";
         }
+        cout << endl;
     }
 }
